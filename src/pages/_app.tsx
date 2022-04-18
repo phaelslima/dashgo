2f39c7cd9e54d726b9b96ad7fc7c90e298010884
Raphael Lima <phaelslima@gmail.com>
2022-04-17 21:16:10 -0300

    feat: configure server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,12 @@ import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext"
 
 import { theme } from "../styles/theme"
 
+import { makeServer } from "../services/mirage"
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
